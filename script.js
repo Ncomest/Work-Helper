4cb8d3fa51a414a2c5874ca5b60f22ed48509454
Hurtik Igor <hurtik.igor@gmail.com>
2023-09-19 15:05:26 +0300

    change name, add animation and dropbox

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let sectionBlocks = document.querySelectorAll('.section-block');
+let sectionBlocks = document.querySelectorAll(".section-block");
 
-sectionBlocks.forEach(function(sectionBlock) {
-  sectionBlock.addEventListener('click', function() {
-    let insideList = sectionBlock.querySelector('.inside-list');
-    insideList.classList.toggle('active');
-  });
+sectionBlocks.forEach(function (sectionBlock) {
+ sectionBlock.addEventListener("click", function () {
+  let insideList = sectionBlock.querySelector(".inside-list");
+  insideList.classList.toggle("active");
+
+  let arrow = document.querySelector(".section-block-text");
+
+  arrow.textContent === "ФВ выпечка: ▼"
+   ? (arrow.textContent = "ФВ выпечка: ▲")
+   : (arrow.textContent = "ФВ выпечка: ▼");
+ });
 });
